507646aee973b20b354f01bb81cc799e793a668c
Cecy Flores <anacecyflores1@gmail.com>
2022-06-17 21:07:52 -0500

    Saves user input and creates it as a note on the left column.

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
-// Dependencies
+const fs = require("fs");
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js");
-
-// Create an express server
+const path = require("path");
 const app = express();
-
-// Create a port
 const PORT = process.env.PORT || 5000;
 
-// Sets up the Express app for data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notelength = noteList.length.toString();
+
+  newNote.id = notelength;
+
+  noteList.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteId = req.params.id.toString();
+
+  noteList = noteList.filter((selected) => {
+    return selected.id != noteId;
+  });
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
